63e860a1655b7d597cdb080a5b1d34ab12aad2de
hiccup1991 <hiccup1991@hotmail.com>
2020-04-08 08:31:25 +0900

    autowire members of the target class

--- src/main/java/jp/co/jti/ims/tc/batch/TCBA0001.java
+++ src/main/java/jp/co/jti/ims/tc/batch/TCBA0001.java
@@ -1,21 +1,18 @@
 package jp.co.jti.ims.tc.batch;
+
 import java.util.List;
 import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 
-import jp.co.jti.ims.tc.batch.TestRepo;
-
-@Controller()
+@Component
 public class TCBA0001 {
 	@Autowired
 	private TestRepo repo;
 
-
-	public static void main(String[] args) {	
-		TCBA0001 tcba0001 = new TCBA0001();
+	public void main(String[] args) {	
 		System.out.println(args[0]);
-		List<HashMap> oj = tcba0001.repo.testSelect(args[0]);
+		List<HashMap> oj = this.repo.testSelect(args[0]);
 		
 		System.out.println(oj);	
 		System.out.println(oj.get(0).get("FIRST_NAME"));
